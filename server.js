78cdff588dc6f25cc0043355d52adfd957ed8e24
Yashvi Soni <yashvisoni87@gmail.com>
2025-06-10 23:51:46 +0400

    for deploying website

--- server.js
+++ server.js
@@ -105,4 +105,5 @@ app.post("/api/contact", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
